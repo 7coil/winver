bd0a37a460b40ddeeef51e4c0c754777abc929ae
Leondro Lio <github@leondrolio.com>
2021-12-01 07:38:33 +0000

    First Commit

---
+++ src/main/java/com/leondrolio/mc/winver/mixin/WinverHud.java
@@ -0,0 +1,20 @@
+package com.leondrolio.mc.winver.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.util.math.MatrixStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(InGameHud.class)
+public class WinverHud {
+  @Inject(method = "render", at=@At("RETURN"), cancellable = true)
+  public void onRender(MatrixStack matrices, float tickDelta, CallbackInfo info) {
+    if (!MinecraftClient.getInstance().options.debugEnabled) {
+      String text = String.format("Minecraft %s", MinecraftClient.getInstance().getGame().getVersion().getName());
+      MinecraftClient.getInstance().textRenderer.drawWithShadow(matrices, text, 2, 2, -1);
+    }
+  }
+}
